1a06fa2dd63a956cbf7f335546f2d5f0ad0d3205
andresbravog <andresbravog@gmail.com>
2013-04-30 16:48:14 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source :rubygems
+
+gem 'rack'
+gem 'nanoc'
+gem 'powder'
+gem 'kramdown'
+gem 'adsf'
+gem 'fssm'
+gem 'compass'
---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path    = "/" 
+project_path = "." 
+css_dir      = "public/stylesheets/" 
+sass_dir     = "content/stylesheets/" 
+images_dir   = "public/images/"
+
+sass_options = {
+	  :syntax => :scss
+}
